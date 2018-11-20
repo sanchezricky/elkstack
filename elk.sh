f7a084282e473ec8aeb6080d5b44bc7630a1b94c
Ricky Sanchez <rickysanchez4130@gmail.com>
2018-11-19 21:37:33 -0800

    Moving over to vagrant

--- elk.sh
+++ elk.sh
@@ -6,51 +6,51 @@ set -u
 # This script will install the ELK stack, Java 8 and Nginx
 
 # Adding Java 8 to apt
-sudo add-apt-repository -y ppa:webupd8team/java
-sudo apt-add-repository -y ppa:webupd8team/java
+add-apt-repository -y ppa:webupd8team/java
+apt-add-repository -y ppa:webupd8team/java
 
 # Importing Elasticsearch public GPG key into APT
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
 
 # Adding Elastic source list to the sources.list.d dir
-echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-6.x.list
+echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-6.x.list
 
 # Update apt
-sudo apt update
+apt-get update
 
 # Silent install for Java 8
 echo debconf shared/accepted-oracle-license-v1-1 select true | \
-  sudo debconf-set-selections
+  debconf-set-selections
 echo debconf shared/accepted-oracle-license-v1-1 seen true | \
-  sudo debconf-set-selections
+  debconf-set-selections
 
 # Install Java 8
-sudo apt install oracle-java8-installer -y
+apt-get install oracle-java8-installer -y
 
 # Installing Nginx
-sudo apt install nginx -y
+apt-get install nginx -y
 
 # UFW allow Nginx HTTP
-sudo ufw allow 'Nginx HTTP'
+# ufw allow 'Nginx HTTP'
 
 # Enable UFW
-sudo ufw --force enable
+# ufw --force enable
 
 # Install Elasticsearch
-sudo apt install elasticsearch
+apt-get install elasticsearch
 
 # Editing Elasticsearch cfg file
-sudo sed -i "/#network.host/c\network.host: localhost" /etc/elasticsearch/elasticsearch.yml
+sed -i "/#network.host/c\network.host: 192.168.56.20" /etc/elasticsearch/elasticsearch.yml
 
 # Start elasticsearch
-sudo systemctl start elasticsearch
+systemctl start elasticsearch
 
 # Enable Elasticsearch to run when system boots
-sudo systemctl enable elasticsearch
+systemctl enable elasticsearch
 
 # Install Kibana
-sudo apt install kibana
+apt-get install kibana
 
 # Enable and start Kibana
-sudo systemctl enable kibana
-sudo systemctl start kibana
+systemctl enable kibana
+systemctl start kibana
