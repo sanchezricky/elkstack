11b47276895a8da675015c76c70409ddf2d719d3
Ricky Sanchez <rickysanchez4130@gmail.com>
2018-12-03 21:28:23 -0800

    initial nginx install

---
+++ nginx/nginx.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# Nginx install
+apt-get install nginx
+
+# Update APT
+apt-get update -y
+
+# Install Nginx
+apt-get install nginx -y
+
+# Open port 80 for Nginx
+ufw allow 'Nginx HTTP'
+
+# Enable the firewall
+echo "y" | ufw enable
+
+# Enable SSH
+ufw allow OpenSSH
